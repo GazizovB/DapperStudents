85bfd3cfc1b7a9dbb5b5a7191ccb679dbd1f2486
Бауыржан <@->
2019-05-01 01:43:55 +0600

    Добавьте файлы проекта.

---
+++ DapperStudents.ConsoleApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using DapperStudents.Models;
+using DapperStudents.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperStudents.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tГлавное меню");
+            Console.WriteLine("Выберите:");
+            Console.WriteLine("Добавление (нажмите 1)");
+            Console.WriteLine("Удаление (нажмите 2)");
+
+            StudentRepository _userRep = new StudentRepository();
+
+            int choise = int.Parse(Console.ReadLine());
+            if (choise == 1)
+            {
+                do
+                {
+                    Console.Write("Фамилия: ");
+                    var lastName = Console.ReadLine();
+                    Console.Write("Имя: ");
+                    var firstName = Console.ReadLine();
+                    Console.Write("Очество: ");
+                    var middleName = Console.ReadLine();
+                    Console.Write("GroupId: ");
+                    int groupId = int.Parse(Console.ReadLine());
+
+
+                    _userRep.InsertStudent(@"INSERT INTO Students (LastName, FirstName, MiddleName, GroupId)
+                                        VALUES(@LastName, @FirstName, @MiddleName, @GroupId)",
+                new Student()
+                {
+                    LastName = lastName,
+                    FirstName = firstName,
+                    MiddleName = middleName,
+                    GroupId = groupId
+                });
+
+                    var students = _userRep.GetAllStudent("SELECT * FROM Students");
+
+                    students.ForEach(f =>
+                    {
+                        Console.WriteLine($"{f.Id}\t\t{f.LastName}\t\t{f.FirstName}\t\t{f.MiddleName}");
+                    });
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
+
+            else if (choise == 2)
+            {
+                do
+                {
+                    _userRep.GetAllStudent("SELECT * FROM Students").ForEach(f =>
+                    {
+                        Console.WriteLine($"{f.Id}\t\t{f.LastName}\t\t{f.FirstName}\t\t{f.MiddleName}");
+                    });
+                    Console.WriteLine("Введите идентификатор для удаления:");
+                    var tmp = Console.ReadLine();
+                    int IdSt;
+                    int.TryParse(tmp, out IdSt);
+
+                    _userRep.DeleteStudent(@"DELETE Students WHERE Id = @Id", IdSt);
+
+                    var students = _userRep.GetAllStudent("SELECT * FROM Students");
+                    students.ForEach(f =>
+                    {
+                        Console.WriteLine($"{f.Id}\t\t{f.LastName}\t\t{f.FirstName}\t\t{f.MiddleName}");
+                    });
+
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
+        }
+    }
+}
---
+++ DapperStudents.DataAccess/StudentRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DapperStudents.Models;
+
+namespace DapperStudents.DataAccess
+{
+    public class StudentRepository
+    {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\бауыржан\мой документы\visual studio 2015\Projects\DapperStudents.ConsoleApp\DapperStudents.DataAccess\StudentDB.mdf;Integrated Security=True";
+
+        //Method InsertStudent
+        public string InsertStudent(string query, Student student)
+        {
+            using (var sql = new SqlConnection(connectionString))
+            {
+                var result = sql.Execute(query, student);
+                if (result < 1) throw new Exception("Ошибка при вставке записи студента");
+            }
+            return "Вставка произошла успешно";
+        }
+
+        //Method Delete
+        public void DeleteStudent(string query, int id)
+        {
+            using (var sql = new SqlConnection(connectionString))
+            {
+
+                sql.Execute(query, new { Id = id });
+            }
+            //return "Удаление произошла успешно";
+        }
+
+        //Method Update
+        //public void UpdateStudent(string query, int id)
+        //{
+        //    using (var sql = new SqlConnection(connectionString))
+        //    {
+        //        var sqlQuery = "UPDATE Students SET Name = @Name, Age = @Age WHERE Id = @Id";
+        //        //sqlQuery.Execute(sqlQuery, user);
+        //    }
+        //}
+
+
+        //Create method GetAllStudent
+        public List<Student> GetAllStudent(string query)
+        {
+            using (var sql = new SqlConnection(connectionString))
+            {
+                return sql.Query<Student>(query).ToList();
+            }
+        }
+
+        public int Insert<T>(string query, T data)
+        {
+            using (var sql = new SqlConnection(connectionString))
+            {
+                return sql.Execute(query, data);
+            }
+        }
+    }
+}
